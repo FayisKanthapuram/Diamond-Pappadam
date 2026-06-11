a59abcf37c7880470fa1c2df7162f6a87201938d
FayisKanthapuram <fayiskanthapuram@gmail.com>
2026-06-11 12:50:13 +0530

    test render

--- backend/src/index.js
+++ backend/src/index.js
@@ -40,7 +40,7 @@ app.use(errorHandler);
 async function start() {
   await connectDB();
   app.listen(env.port, () => {
-    console.log(`Server running on http://localhost:${env.port} (PORT=${env.port})`);
+    console.log(`Server is running on http://localhost:${env.port} (PORT=${env.port})`);
   });
 }
 
